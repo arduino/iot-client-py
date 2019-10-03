01c99ba69c3d83d4f3296dfc0f9fa79685f587eb
Massimiliano Pippi <m.pippi@arduino.cc>
2019-10-03 18:11:17 +0200

    bump to beta3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "arduino-iot-client"
-VERSION = "1.0.0-beta2"
+VERSION = "1.0.0-beta3"
 # To install the library, run the following
 #
 # python setup.py install
