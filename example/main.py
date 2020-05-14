8e02e3b1b34d6fc7b3dcc87c926b02422abed18d
Roberto Sora <r.sora@arduino.cc>
2020-05-14 13:25:58 +0200

    Setup smoke test using example (#39)
    * Add idea metadata folder in gitignore
    
    * Add smoke test using python client example
    
    * Remove duplicated pip install
    
    * Remove pip freeze
    
    * Remove client debug

--- example/main.py
+++ example/main.py
@@ -1,11 +1,13 @@
+import os
+
 import iot_api_client as iot
 
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
 
-CLIENT_ID = ""  # get a valid one from your Arduino Create account
-CLIENT_SECRET = ""  # get a valid one from your Arduino Create account
+CLIENT_ID = os.getenv("CLIENT_ID")  # get a valid one from your Arduino Create account
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")  # get a valid one from your Arduino Create account
 
 
 if __name__ == "__main__":
@@ -30,7 +32,7 @@ if __name__ == "__main__":
     # Now we setup the iot-api Python client, first of all create a
     # configuration object. The access token goes in the config object.
     client_config = iot.Configuration(host="https://api2.arduino.cc/iot")
-    client_config.debug = True
+    # client_config.debug = True
     client_config.access_token = token.get("access_token")
 
     # Create the iot-api Python client with the given configuration
